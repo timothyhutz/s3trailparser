720569666924a86dbe3e399e767e21504476cc40
Timothy Hutz <timothy.hutz@pearson.com>
2018-03-28 17:01:58 -0500

    Working event intake handeler and file retrive with unziping and decoding properly...

--- s3trailparser.py
+++ s3trailparser.py
@@ -1,5 +1,24 @@
 """Written by Timothy Hutz <timothyhutz@gmail.com
-for use in AWS lambda only
+for use in AWS lambda only on python 3.6 runtime
 """
 
-def main()
+import boto3
+import gzip
+
+
+class DataGz(object):
+	def __init__(self, region):
+		self.region = region
+		self.s3 = boto3.client('s3', region_name=self.region)
+
+	def main(self, bucket, key):
+		return self.s3.get_object(Bucket=bucket, Key=key)['Body']
+
+
+def main(event, context):
+	for record in event['Records']:
+		filedata = DataGz(region=record['awsRegion']).main(key=record['s3']['object']['key'], bucket=record['s3']['bucket']['name'])
+		unzipstream = gzip.open(filedata, 'rb')
+		streamdata = unzipstream.read().decode('utf8')
+		unzipstream.close()
+		print(streamdata)

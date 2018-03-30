4a4b79429c9d323d81c64570399a2af0920e7ebd
Timothy Hutz <timothy.hutz@pearson.com>
2018-03-30 11:43:14 -0500

    Added logging handelers and updated README with AWS requirements.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # s3trailparser
 Parses Json Cloudtrail data into an elastic search cluster
+
+<h2>Requires following lambda setup..</h2>
+512-768 MB of memory preset
+1 minutes runtime timeout
+
+<h2>Following AWS services needed </h2>
+Aurora DB
+
--- s3trailparser.py
+++ s3trailparser.py
@@ -4,6 +4,20 @@ for use in AWS lambda only on python 3.6 runtime
 
 import boto3
 import gzip
+import logging
+
+LOG_LEVEL = None
+if LOG_LEVEL is None:
+	log_config_level = logging.INFO
+else:
+	log_config_level = LOG_LEVEL
+
+logging.getLogger().setLevel(log_config_level)
+
+ELASTISEARCHURL = None
+if ELASTISEARCHURL is None:
+	logging.error('Missing Elastic Search Endpoint ')
+	exit(1)
 
 
 class DataGz(object):
@@ -12,13 +26,39 @@ class DataGz(object):
 		self.s3 = boto3.client('s3', region_name=self.region)
 
 	def main(self, bucket, key):
-		return self.s3.get_object(Bucket=bucket, Key=key)['Body']
+		try:
+			action = self.s3.get_object(Bucket=bucket, Key=key)['Body']
+			logging.debug(action)
+			return action
+		except Exception as message:
+			logging.error(message)
+			exit(2)
+
+class ESload(object):
+	def __init__(self):
+
+		self.url = ELASTISEARCHURL
+
+	def __call__(self, *args, **kwargs):
+		pass
+
 
 
 def main(event, context):
+	logging.debug(event)
 	for record in event['Records']:
 		filedata = DataGz(region=record['awsRegion']).main(key=record['s3']['object']['key'], bucket=record['s3']['bucket']['name'])
-		unzipstream = gzip.open(filedata, 'rb')
-		streamdata = unzipstream.read().decode('utf8')
+		logging.info(record['awsRegion'] )
+		logging.info(['bucket=' + record['s3']['bucket']['name'],'File=' + record['s3']['object']['key']])
+		unzipstream = None
+		try:
+			unzipstream = gzip.open(filedata, 'rb')
+		except Exception as message:
+			logging.error(message)
+			exit(3)
+		try:
+			streamdata = unzipstream.read().decode('utf8')
+		except Exception as message:
+			logging.error(message)
+			exit(4)
 		unzipstream.close()
-		print(streamdata)

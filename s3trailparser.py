ccf0ac1131bdeac3705a460daf51e9cc40f70472
Timothy Hutz <timothy.hutz@pearson.com>
2018-03-30 12:05:01 -0500

    Added comments and Elastic Search work was started..

--- s3trailparser.py
+++ s3trailparser.py
@@ -7,7 +7,7 @@ import gzip
 import logging
 
 LOG_LEVEL = None
-if LOG_LEVEL is None:
+if LOG_LEVEL is None: # Presets the Logging Level if you didn't define it in ENV variables
 	log_config_level = logging.INFO
 else:
 	log_config_level = LOG_LEVEL
@@ -15,28 +15,29 @@ else:
 logging.getLogger().setLevel(log_config_level)
 
 ELASTISEARCHURL = None
-if ELASTISEARCHURL is None:
-	logging.error('Missing Elastic Search Endpoint ')
+if ELASTISEARCHURL is None: # Fails app because Elastic Search is a critical component to this
+	logging.critical('Missing Elastic Search Endpoint ')
 	exit(1)
 
 
-class DataGz(object):
-	def __init__(self, region):
+class DataGz(object): # This class gets the S3 GZ object and returns the body data to memory space variable.
+	def __init__(self, region): # Prebuilds the Client connection
 		self.region = region
 		self.s3 = boto3.client('s3', region_name=self.region)
 
-	def main(self, bucket, key):
+	def main(self, bucket, key): # This is the S3 Object call
 		try:
 			action = self.s3.get_object(Bucket=bucket, Key=key)['Body']
 			logging.debug(action)
 			return action
 		except Exception as message:
-			logging.error(message)
+			logging.critical(message)
 			exit(2)
 
-class ESload(object):
-	def __init__(self):
 
+class ESload(object): # This class parses the data and pushes it to the indexer...
+	def __init__(self, streamdata):
+		self.streamdata = streamdata
 		self.url = ELASTISEARCHURL
 
 	def __call__(self, *args, **kwargs):
@@ -49,7 +50,7 @@ def main(event, context):
 	for record in event['Records']:
 		filedata = DataGz(region=record['awsRegion']).main(key=record['s3']['object']['key'], bucket=record['s3']['bucket']['name'])
 		logging.info(record['awsRegion'] )
-		logging.info(['bucket=' + record['s3']['bucket']['name'],'File=' + record['s3']['object']['key']])
+		logging.info(['bucket= ' + record['s3']['bucket']['name'],'File= ' + record['s3']['object']['key']])
 		unzipstream = None
 		try:
 			unzipstream = gzip.open(filedata, 'rb')
